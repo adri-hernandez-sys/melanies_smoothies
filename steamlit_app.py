fed2ac5375f4fde040c927ac40f255f0ba8e644b
adri-hernandez-sys <adri.hernandez@sdggroup.com>
2026-06-22 12:43:18 +0200

    Update steamlit_app.py

--- steamlit_app.py
+++ steamlit_app.py
@@ -45,6 +45,6 @@ if ingredients_list:
 
 # Display nutrition info
 smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json() )
 
 
